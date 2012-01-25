fbb29cec6c6ce8731e331182cd5e90a6cdf59212
piumosso <golikoffartem@gmail.com>
2012-01-25 15:22:35 +0400

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='django-wtf',
+    version = '0.1',
+    description='Django wtf utils',
+    author='Artem Golikov',
+    author_email='golikoffartem@gmail.com',
+    url='http://github.com/piumosso/django-wtf',
+)
