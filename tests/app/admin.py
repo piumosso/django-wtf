5d08ef7ddacce1b6d7fd6ddd806d8921228dc9d7
piumosso <golikoffartem@gmail.com>
2012-03-20 08:47:32 +0400

    Paginator refactoring

--- tests/app/admin.py
+++ tests/app/admin.py
@@ -5,6 +5,7 @@ from app.models import Model
 class ModelAdmin(admin.ModelAdmin):
     list_filter = ['boolean', 'date']
     list_display = ['char', 'integer', 'email', 'date']
+    list_per_page = 2
 
 
 admin.site.register(Model, ModelAdmin)
