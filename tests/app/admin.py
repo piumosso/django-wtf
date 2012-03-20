f2b499c1ad3c7ee7e971567660bfaf74d364ddc5
piumosso <golikoffartem@gmail.com>
2012-03-20 08:26:09 +0400

    Results table refactoring

--- tests/app/admin.py
+++ tests/app/admin.py
@@ -4,6 +4,7 @@ from app.models import Model
 
 class ModelAdmin(admin.ModelAdmin):
     list_filter = ['boolean', 'date']
+    list_display = ['char', 'integer', 'email', 'date']
 
 
 admin.site.register(Model, ModelAdmin)
