1ae39d10bbfdb9021c0b3763fd6ab72ec89b34e6
piumosso <golikoffartem@gmail.com>
2012-03-29 08:07:00 +0400

    Form field refactoring started

--- tests/app/admin.py
+++ tests/app/admin.py
@@ -6,6 +6,23 @@ class ModelAdmin(admin.ModelAdmin):
     list_filter = ['boolean', 'date']
     list_display = ['char', 'integer', 'email', 'date']
     list_per_page = 2
+    fieldsets = (
+        (None, {
+            'fields': ('char', 'decimal', 'float', 'integer', 'email', 'ip', 'text'),
+            'description': 'Common string and digital fields'
+        }),
+        ('Boolean', {
+            'classes': ('collapse',),
+            'fields': ('boolean', 'null_boolean'),
+            'description': 'Boolean fields'
+        }),
+        ('Dates', {
+            'fields': ('date', 'time', 'datetime'),
+        }),
+        ('Files', {
+            'fields': ('file', 'file_path', 'image'),
+        }),
+    )
 
 
 admin.site.register(Model, ModelAdmin)
