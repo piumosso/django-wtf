f4d8887004457ede123dd9bdcae956c4ae03d7b0
piumosso <golikoffartem@gmail.com>
2012-04-01 11:53:15 +0400

    Form field refactoring (collapsed fieldsets)

--- django_wtf/admin/static/admin/js/collapse.js
+++ django_wtf/admin/static/admin/js/collapse.js
@@ -1,16 +1,16 @@
 (function($) {
 	$(document).ready(function() {
 		// Add anchor tag for Show/Hide link
-		$("fieldset.collapse").each(function(i, elem) {
+		$(".form__fieldset.collapse").each(function(i, elem) {
 			// Don't hide if fields in this fieldset have errors
 			if ($(elem).find(".form__row_errors").length == 0) {
-				$(elem).addClass("collapsed").find("h2").first().append(' (<a id="fieldsetcollapser' +
-					i +'" class="collapse-toggle" href="#">' + gettext("Show") +
+				$(elem).addClass("collapsed").find(".module__header").first().append(' (<a id="fieldsetcollapser' +
+					i +'" class="form__fieldset-collapse-toggle" href="#">' + gettext("Show") +
 					'</a>)');
 			}
 		});
 		// Add toggle to anchor tag
-		$("fieldset.collapse a.collapse-toggle").toggle(
+		$(".form__fieldset.collapse .form__fieldset-collapse-toggle").toggle(
 			function() { // Show
 				$(this).text(gettext("Hide")).closest("fieldset").removeClass("collapsed").trigger("show.fieldset", [$(this).attr("id")]);
 				return false;
